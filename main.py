77483e4bf849f48759dc031dc399adef73c90075
1dars <jumadullaeviskandar@gmail.com>
2023-11-27 23:25:36 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+print("Hello word")
+print( 5**4 )
+print( 22%4 )
+# Kvadratning tomoni
+s = 125
+
+# Yuzi
+A = s**2
+
+# Perimetri
+P = 4 * s
+
+# Natijalarni chiqarish
+print( f"Kvadratning yuzi: {A}" )
+print( f"Kvadratning perimetri: {P}" )
+# Diametri berilgan doira
+diameter = 12
+
+# Radiusni hisoblash
+radius = diameter / 2
+
+# pi qiymati
+pi = 3.14
+
+# Doiraning yuzi
+area = pi * radius**2
+
+# Natijani chiqarish
+print( f"Doiraning yuzi: {area}" )
